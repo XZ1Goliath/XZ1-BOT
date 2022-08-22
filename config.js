cd62bf241b16ae74152f4357353fd364cf8a1012
XZ1Goliath <62725988+XZ1Goliath@users.noreply.github.com>
2022-08-22 21:48:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,12 +9,12 @@ const chalk = require('chalk')
 
 // Website Api
 global.APIs = {
-	zenz: 'https://zenzapis.xyz',
+	zenz: 'https://xteam.xyz',
 }
 
 // Apikey Website Api
 global.APIKeys = {
-	'https://zenzapis.xyz': 'Your Key',
+	'https://xteam.xyz': '8cb285bb8c355c09',
 }
 
 // Other
